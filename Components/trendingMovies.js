801aeddd714748d5f7470252b4deaa2d2a190b02
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2024-05-02 09:45:05 +0600

    modify tending movies component

--- Components/trendingMovies.js
+++ Components/trendingMovies.js
@@ -1,16 +1,21 @@
 import { Dimensions, StyleSheet, Text, TouchableWithoutFeedback, View,Image} from 'react-native'
 import React from 'react'
 import Carousel from 'react-native-snap-carousel'
+import { useNavigation } from '@react-navigation/native'
 
 var {width,height}=Dimensions.get('window')
 
 const TrendingMovies = ({data}) => {
+  const navigation = useNavigation();
+  const handleClick=()=>{
+    navigation.navigate('Movie',item)
+  }
   return (
     <View className='md-8'>
       <Text className='text-white text-xl mx-4 mb-5'>Trending</Text>
       <Carousel
         data ={data}
-        renderItem={({item})=> <MovieCard item={item}/>}
+        renderItem={({item})=> <MovieCard item={item} handleClick={handleClick}/>}
         firstItem={1}
         inactiveSlideOpacity={0.6}
         sliderWidth={width}
@@ -25,9 +30,9 @@ export default TrendingMovies
 
 const styles = StyleSheet.create({})
 
-const MovieCard=({item})=>{
+const MovieCard=({item, handleClick})=>{
   return(
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={handleClick}>
       <Image
           source={require('../assets/adaptive-icon.png')}
           style={{
